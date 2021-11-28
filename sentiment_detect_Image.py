9ff6b41fd33a742907070341d43dbd9f5539c393
Messaouali Abdeslem <74242810+abdeslem1803@users.noreply.github.com>
2021-11-28 13:42:48 +0100

    update

--- sentiment_detect_Image.py
+++ sentiment_detect_Image.py
@@ -28,19 +28,18 @@ image = frame.copy()
 #detect faces in the frame and preict if their sentiment
     #extract coordination of faces
 (coordination,sentiment_face_detection)=sentiment_from_faces(image,face_detection_model,classifier)
-    #loop over the detected face and their corrosponding coordinations
 for (box,prediction) in zip(coordination,sentiment_face_detection):
     (start_x,start_y,end_x,end_y)=box
     sentiment=np.argmax(prediction)
     resultat= ["angry","disgust","fear","happy","neutral","sad","surprise"]
      
         #determine the class label and color we will use to draw the bounding box and text
-label=resultat[sentiment]
-color=(0,255,0) 
+    label=resultat[sentiment]
+    color=(0,255,0) 
             
         #display the label and bounding boxes
-cv2.putText(image,label,(start_x,start_y-10),cv2.FONT_HERSHEY_SIMPLEX,0.45,color,2)
-cv2.rectangle(image,(start_x,start_y),(end_x,end_y),color,2)
+    cv2.putText(image,label,(start_x,start_y-10),cv2.FONT_HERSHEY_SIMPLEX,0.45,color,2)
+    cv2.rectangle(image,(start_x,start_y),(end_x,end_y),color,2)
         
 cv2.imshow("OutPut.jpg",image)
 cv2.waitKey(0)
